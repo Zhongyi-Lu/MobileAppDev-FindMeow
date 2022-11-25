1a329c730f4339ec09d43449eb1959ecf6ac7b01
Zhongyi-Lu <zhongyi_lu@outlook.com>
2022-11-24 17:31:52 -0800

    feat: temp. fetch data only when scrolling to the top

--- components/screens/DiscoverMainScreen.jsx
+++ components/screens/DiscoverMainScreen.jsx
@@ -1,6 +1,7 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import {
   collection,
+  getDocs,
   onSnapshot, orderBy, query
 } from "firebase/firestore";
 import React, { useEffect, useRef, useState } from "react";
@@ -15,106 +16,117 @@ import CatInformation from "./CatInformation";
 import DiscoverFilter from "./DiscoverFilter";
 
 export default function DiscoverMainScreen({ route, navigation }) {
-  const [selectedIndex, setSelectedIndex] = useState(0);
-
-
-  /* values used for DiscoverFilter start */
-  const [visible, setVisible] = useState(false);
-  const [value, setValue] = useState(0);
-
-  const [selectedBreed, setSelectedBreed] = useState("All");
-  const [selectedAge, setSelectedAge] = useState("All");
-  const [selectedState, setSelectedState] = useState("All");
-  const [selectedGender, setSelectedGender] = useState("All");
-
-  const refRBSheet = useRef();
-  /* values used for DiscoverFilter end */
-
-
-  function resetAllFilters() {
-    setValue(0);
-
-    setSelectedBreed("");
-    setSelectedAge("");
-    setSelectedState("");
-    setSelectedGender("");
-  }
-
-
-  /* data collector used for top filter tags - start */
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    let q;
-    // 1. Newer Post  
-    if (selectedIndex == 0) {
-      q = query(collection(db, "Cats"), orderBy("UploadTime", "desc"));
-    }
-    // 2. Nearby Post
-    else if (selectedIndex == 1) {
-      q = query(collection(db, "Cats"), orderBy("UploadTime", "desc"));
-      // todo ...
-    }
-    // 3. Lower Price
-    else if (selectedIndex == 2) {
-      q = query(collection(db, "Cats"), orderBy("Price", "desc"));
-    }
-    const unSubscribe = onSnapshot(q, (snapshot) => {
-      setData(
-        snapshot.docs.map((entry) => {
-          const birthday = new Date(entry.data().Birthday);
-          const now = new Date();
-          let age =
-            now.getMonth() -
-            birthday.getMonth() +
-            12 * (now.getFullYear() - birthday.getFullYear());
-          // age cannot be negative
-          if (age === undefined || isNaN(age) || age < 0) {
-            age = 0;
-          }
-
-          return {
-            id: entry.id,
-            name: entry.data().Breed,
-            sex: entry.data().Gender,
-            price: entry.data().Price,
-            month: age,
-            photo: entry.data().Picture,
-            cattery: entry.data().Cattery,
-            uploadTime: entry.data().UploadTime,
-          };
-        })
-      );
-    });
-
-    return () => unSubscribe();
-  }, []);
-  /* data collector used for top filter tags - end */
-
-
-  /* events for top filter tags - start */
-  const onFilterChange = (value) => {
-    let dataCopy = data;
-    // 1. newer post
-    if (value === 0) {
-      setData(dataCopy.sort((d1, d2) => d2.uploadTime - d1.uploadTime));
-    }
-    // 2. nearby Post
-    else if (value === 1) {
-      Alert.alert("Feature for this button is coming soon~", "See you next time!", [
-        { text: "Sad" },
-        { text: "Wait for you" },
-      ])
-    }
-    // 3. Lower Price
-    else if (value === 2) {
-      setData(dataCopy.sort((d1, d2) => d1.price - d2.price));
-    }
-    setSelectedIndex(value);
-  };
-  /* events for top filter tags - end */
-
-
   function MainScreen({ route, navigation }) {
+    const [selectedIndex, setSelectedIndex] = useState(0);
+
+
+    /* values used for DiscoverFilter start */
+    const [visible, setVisible] = useState(false);
+    const [value, setValue] = useState(0);
+
+    const [selectedBreed, setSelectedBreed] = useState("All");
+    const [selectedAge, setSelectedAge] = useState("All");
+    const [selectedState, setSelectedState] = useState("All");
+    const [selectedGender, setSelectedGender] = useState("All");
+
+    const refRBSheet = useRef();
+    /* values used for DiscoverFilter end */
+
+
+    function resetAllFilters() {
+      setValue(0);
+
+      setSelectedBreed("");
+      setSelectedAge("");
+      setSelectedState("");
+      setSelectedGender("");
+    }
+
+
+    /* data collector used for top filter tags - start */
+    const [data, setData] = useState([]);
+
+    if (data.length === 0) {
+      console.log("fetch data because data is empty");
+      fetchCatData();
+    }
+
+
+    async function fetchCatData() {
+      console.log("fetch data once");
+      let q;
+      // 1. Newer Post  
+      if (selectedIndex == 0) {
+        q = query(collection(db, "Cats"), orderBy("UploadTime", "desc"));
+      }
+      // 2. Nearby Post
+      else if (selectedIndex == 1) {
+        q = query(collection(db, "Cats"), orderBy("UploadTime", "desc"));
+        // todo ...
+      }
+      // 3. Lower Price
+      else if (selectedIndex == 2) {
+        q = query(collection(db, "Cats"), orderBy("Price", "desc"));
+      }
+
+      let newData = [];
+      const querySnapshot = await getDocs(q);
+
+      querySnapshot.forEach((entry) => {
+        const birthday = new Date(entry.data().Birthday);
+        const now = new Date();
+        let age =
+          now.getMonth() -
+          birthday.getMonth() +
+          12 * (now.getFullYear() - birthday.getFullYear());
+        // age cannot be negative
+        if (age === undefined || isNaN(age) || age < 0) {
+          age = 0;
+        }
+
+        newData.push({
+          id: entry.id,
+          name: entry.data().Breed,
+          sex: entry.data().Gender,
+          price: entry.data().Price,
+          month: age,
+          photo: entry.data().Picture,
+          cattery: entry.data().Cattery,
+          uploadTime: entry.data().UploadTime,
+        });
+      })
+
+      setData(newData);
+    }
+    /* data collector used for top filter tags - end */
+
+
+    /* events for top filter tags - start */
+    const onFilterChange = (value) => {
+      let dataCopy = data;
+      // 1. newer post
+      if (value === 0) {
+        setData(dataCopy.sort((d1, d2) => d2.uploadTime - d1.uploadTime));
+      }
+      // 2. nearby Post
+      else if (value === 1) {
+        Alert.alert("Feature for this button is coming soon~", "See you next time!", [
+          { text: "Sad" },
+          { text: "Wait for you" },
+        ])
+      }
+      // 3. Lower Price
+      else if (value === 2) {
+        setData(dataCopy.sort((d1, d2) => d1.price - d2.price));
+      }
+      setSelectedIndex(value);
+    };
+    /* events for top filter tags - end */
+
+    function ifCloseToTop({ layoutMeasurement, contentOffset, contentSize }) {
+      return contentOffset.y < 0;
+    }
+
     return (<View
       style={{
         paddingHorizontal: 16,
@@ -191,6 +203,11 @@ export default function DiscoverMainScreen({ route, navigation }) {
           renderItem={({ item, index }) => <CatCard cat={item} navigation={navigation} />}
           numColumns={2}
           ListFooterComponent={<View style={{ height: 80 }} />}
+          onScrollEndDrag={({ nativeEvent }) => {
+            console.log(nativeEvent.contentOffset);
+            if (ifCloseToTop(nativeEvent))
+              fetchCatData()
+          }}
         />
       </View>
     </View>)
